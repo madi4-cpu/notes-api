227a28add0fbc458cadbe5fa95984847898173a3
Madikara <madikaragoy@gmail.com>
2026-09-17 11:11:19 +0500

    test_5

--- notes/models.py
+++ notes/models.py
@@ -22,3 +22,11 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
